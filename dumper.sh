e1c182f86253c5e53e843d8ba9a34e9e50f8f66d
Rama Bondan Prakoso <ramanarubp@gmail.com>
2023-12-28 04:54:59 +0700

    Update dumper.sh

--- dumper.sh
+++ dumper.sh
@@ -1243,8 +1243,8 @@ elif [[ -s "${PROJECT_DIR}"/.gitlab_token ]]; then
 	git checkout -b "${branch}" || { git checkout -b "${incremental}" && export branch="${incremental}"; }
 	find . \( -name "*sensetime*" -o -name "*.lic" \) | cut -d'/' -f'2-' >| .gitignore
 	[[ ! -s .gitignore ]] && rm .gitignore
-	[[ -z "$(git config --get user.email)" ]] && git config user.email "guptasushrut@gmail.com"
-	[[ -z "$(git config --get user.name)" ]] && git config user.name "Sushrut1101"
+	[[ -z "$(git config --get user.email)" ]] && git config user.email "ramanarubp@gmail.com"
+	[[ -z "$(git config --get user.name)" ]] && git config user.name "Rama Bondan Prakoso"
 
 	# Create Subgroup
 	GRP_ID=$(curl -s --request GET --header "PRIVATE-TOKEN: ${GITLAB_TOKEN}" "${GITLAB_HOST}/api/v4/groups/${GIT_ORG}" | jq -r '.id')
